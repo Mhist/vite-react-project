7a947faef209d7941871458b6078252e8e2d58ea
jiang yadong <18827637106@163.com>
2025-05-30 17:49:56 +0800

    learn jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    const num =  3.141592654;
+    const object= {
+        a:21,
+        b:'other'
+    }
+    return (
+      <>
+          {/*插值语法：可以用于展示字符串、数字、数组（元素为普通类型）、元素、支持API的调用*/}
+          {/*展示序列化的对象*/}
+          <div>插值语法展示字符串：{'艺画开天'}</div>
+          <div>插值语法展示数字：{2025}</div>
+          <div>插值语法展示数字：{['hello world,', ' fight up']}</div>
+          <div>插值语法展示元素：{ <span>测试元素</span>}</div>
+          <div>插值语法展示布尔值 展示不了、显示空：{true}</div>
+          <div>插值语法展示布尔值三元表达式：{true ? 'yes' : 'no'}</div>
+          <div>插值语法展示：API调用 { num.toFixed(2 )}</div>
+          <div>插值语法展示序列化对象：{JSON.stringify(object)}</div>
+      </>
   )
 }
 
