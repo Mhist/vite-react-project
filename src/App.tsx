7ac9f1792b764977d963fee7714de1d675b9acee
jiang yadong <18827637106@163.com>
2025-05-30 23:11:36 +0800

    add event listener

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 
 import './App.css'
+import * as React from "react";
 
 function App() {
-    const num =  3.141592654;
-    const object= {
-        a:21,
-        b:'other'
+    const fn = <T,>(param: T,e:React.MouseEvent) => {
+        console.log('Hello World!',param,e);
     }
     return (
       <>
-          {/*插值语法：可以用于展示字符串、数字、数组（元素为普通类型）、元素、支持API的调用*/}
-          {/*展示序列化的对象*/}
-          <div>插值语法展示字符串：{'艺画开天'}</div>
-          <div>插值语法展示数字：{2025}</div>
-          <div>插值语法展示数字：{['hello world,', ' fight up']}</div>
-          <div>插值语法展示元素：{ <span>测试元素</span>}</div>
-          <div>插值语法展示布尔值 展示不了、显示空：{true}</div>
-          <div>插值语法展示布尔值三元表达式：{true ? 'yes' : 'no'}</div>
-          <div>插值语法展示：API调用 { num.toFixed(2 )}</div>
-          <div>插值语法展示序列化对象：{JSON.stringify(object)}</div>
+          {/*事件：驼峰onClick   如果需要传参传递高阶函数、如果不需要就直接丢给函数体*/}
+          {/*如果想支持泛型呢？ 纠正泛型 <T,> */}
+          <div onClick={(e)=>fn('123', e)}>点击加1</div>  
       </>
   )
 }
